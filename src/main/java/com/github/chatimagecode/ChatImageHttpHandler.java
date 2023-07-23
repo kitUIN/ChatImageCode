17f56222a15d6e5ec88eecefe3294c4a60ef4e4a
Nacho <kulujun@gmail.com>
2023-07-23 17:51:25 +0800

    init

---
+++ src/main/java/com/github/chatimagecode/ChatImageHttpHandler.java
@@ -0,0 +1,59 @@
+package com.github.chatimagecode;
+
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static com.github.chatimagecode.ChatImageHandler.loadFile;
+
+/**
+ * @author kitUIN
+ */
+public class ChatImageHttpHandler {
+    public static HashMap<String, Integer> HTTPS_MAP = new HashMap<>();
+
+    public static boolean getInputStream(String url) {
+        OkHttpClient httpClient = new OkHttpClient();
+        Request getRequest;
+        try {
+            getRequest = new Request.Builder()
+                    .url(url)
+                    .get()
+                    .build();
+        } catch (IllegalArgumentException ep) {
+            return false;
+        }
+        if (HTTPS_MAP.containsKey(url) && HTTPS_MAP.get(url) == 1) {
+            return true;
+        } else {
+            HTTPS_MAP.put(url, 1);
+        }
+        Call call = httpClient.newCall(getRequest);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                HTTPS_MAP.put(url, 2);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                String url = String.valueOf(call.request().url());
+                ResponseBody body = response.body();
+                if (body != null) {
+                    try {
+                        loadFile(body.byteStream(), url);
+                    } catch (IOException ignored) {
+
+                    }
+                }
+                HTTPS_MAP.put(url, 2);
+            }
+        });
+        return true;
+
+    }
+
+
+}
+
