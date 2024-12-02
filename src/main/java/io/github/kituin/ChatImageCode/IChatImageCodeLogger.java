05575404d0c04b21547cd01b9e842a702ec657b4
kituin <kit_uin@outlook.com>
2024-12-02 19:31:22 +0800

    :sparkles: log warn

--- src/main/java/io/github/kituin/ChatImageCode/IChatImageCodeLogger.java
+++ src/main/java/io/github/kituin/ChatImageCode/IChatImageCodeLogger.java
@@ -3,37 +3,61 @@ package io.github.kituin.ChatImageCode;
 public interface IChatImageCodeLogger {
     /**
      * 打印日志
+     *
      * @param log 日志内容
      */
     void info(String log);
+
     /**
      * 打印日志
-     * @param log 日志内容
+     *
+     * @param log  日志内容
      * @param args 参数
      */
     void info(String log, Object... args);
 
     /**
      * 打印日志
+     *
      * @param log 日志内容
      */
     void debug(String log);
+
     /**
      * 打印日志
-     * @param log 日志内容
+     *
+     * @param log  日志内容
      * @param args 参数
      */
     void debug(String log, Object... args);
 
     /**
      * 打印日志
+     *
      * @param log 日志内容
      */
     void error(String log);
+
     /**
      * 打印日志
-     * @param log 日志内容
+     *
+     * @param log  日志内容
      * @param args 参数
      */
     void error(String log, Object... args);
+
+    /**
+     * 打印日志
+     *
+     * @param log 日志内容
+     */
+    void warn(String log);
+
+    /**
+     * 打印日志
+     *
+     * @param log  日志内容
+     * @param args 参数
+     */
+    void warn(String log, Object... args);
 }
